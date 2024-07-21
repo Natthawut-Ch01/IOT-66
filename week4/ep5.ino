27d778795091c7b13c9062d5a6b438a7b4a08767
Junior Developer <150988991+Natthawut-Ch01@users.noreply.github.com>
2024-07-22 01:19:50 +0700

    Create ep5.ino

---
+++ week4/ep5.ino
@@ -0,0 +1,37 @@
+void setup() {
+  pinMode(17,INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_BLUE, OUTPUT);
+  Serial.begin(9600);
+
+}
+//pullup_module
+void loop() {
+  int a;
+  a = digitalRead(17);
+  if(a==0){
+  digitalWrite(LED_BUILTIN,HIGH);
+  led_pat();
+  }
+  else{
+   digitalWrite(LED_BUILTIN,LOW);
+   digitalWrite(LED_RED,HIGH);
+   digitalWrite(LED_GREEN,HIGH);
+   digitalWrite(LED_BLUE,HIGH);
+  }
+  delay(10);
+}
+
+void led_pat(){
+  digitalWrite(LED_RED,LOW);
+  delay(500);
+  digitalWrite(LED_RED,HIGH);
+  digitalWrite(LED_GREEN,LOW);
+  delay(500);
+  digitalWrite(LED_GREEN,HIGH);
+  digitalWrite(LED_BLUE,LOW);
+  delay(500);
+  digitalWrite(LED_BLUE,HIGH);
+}
