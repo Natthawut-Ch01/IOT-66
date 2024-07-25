6f3df194bd2e34cf1a9dffc3d2241ab7290e4163
Nnch <150988991+Natthawut-Ch01@users.noreply.github.com>
2024-07-25 23:15:59 +0700

    Update ep7-5.ino

--- week7/ep7-5.ino
+++ week7/ep7-5.ino
@@ -1,6 +1,6 @@
 void setup() {
   pinMode(2, OUTPUT);
-  analogWriteResolution(10) //8=0-255 Default, 10=0-1023, 12=0-4095
+  analogWriteResolution(10);//8=0-255 Default, 10=0-1023, 12=0-4095
   //Serial.begin(9600);
 
 }
