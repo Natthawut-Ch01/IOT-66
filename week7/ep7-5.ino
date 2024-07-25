64301c2d302ff03dba6a347fc732ce4e6b191aaf
Nnch <150988991+Natthawut-Ch01@users.noreply.github.com>
2024-07-25 23:13:19 +0700

    Create ep7-5.ino

---
+++ week7/ep7-5.ino
@@ -0,0 +1,14 @@
+void setup() {
+  pinMode(2, OUTPUT);
+  analogWriteResolution(10) //8=0-255 Default, 10=0-1023, 12=0-4095
+  //Serial.begin(9600);
+
+}
+
+void loop() {
+  analogWrite(2, 128);
+  delay(50);
+  
+  
+  
+}
